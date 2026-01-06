0ef6060f6ef9a8a3239eb369be74adb53d32a0cf
manticorexl <zip3366@naver.com>
2026-01-06 11:29:17 +0900

    update 1/6 11:29

--- class2/11866.py
+++ class2/11866.py
@@ -6,11 +6,9 @@ n, k = map(int, input().split())
 numbers = []
 result = []
 
-# Insert numbers into the list
 for i in range(1, n + 1):
     numbers.append(i)
 
-# Make Josephus permutation
 while len(numbers) != 0:
     for _ in range(k - 1):
         numbers.append(numbers.pop(0))
